1e499d999acf46250997aefff7dc134b39ca356d
Victor Castro <victor_castro_gomez@hotmail.com>
2018-11-03 15:54:20 -0700

    Python example

---
+++ python_examples/test.py
@@ -0,0 +1,29 @@
+
+
+
+
+print("HELLO WORLD")
+
+for i in range(2,4):
+	print(i)
+
+tuple_months = ('January','February')
+
+for i in tuple_months:
+	print(i)
+list_cats = ['Tom', 'Snappy']
+for i in list_cats:
+	print(i)
+print('{}'.format(tuple_months[0]))
+print(list_cats[0])
+list_cats.append('Catherine')
+print(list_cats[-1])
+dictionary_phonebook = {'Andrew Parson':8806336, \
+'Emily Everett':6784346}
+
+print(dictionary_phonebook)
+print(dictionary_phonebook['Andrew Parson'])
+print(dictionary_phonebook.get('Peter Power'))
+
+
+
